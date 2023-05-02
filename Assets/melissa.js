471c69fb94b48fbf32bd42a121605025c5773f7d
MD <116689775+MelissaD2601@users.noreply.github.com>
2023-05-01 20:25:08 -0500

    Committing changes made to the melissa.js file after restarting the project.

--- Assets/melissa.js
+++ Assets/melissa.js
@@ -1,24 +1,19 @@
-//var audience = getElementById("audience");
-var G = document.getElementById("G");
-var PG = document.getElementById("PG");
-var PG13 = document.getElementById("PG-13");
-var R = document.getElementById("R");
-var NC17 = document.getElementById("NC-17");
 
-var optionG = document.getElementById("G");
-var optionPG = document.getElementById("PG");
-var optionPG13 = document.getElementById("PG13");
-var optionR = document.getElementById("R");
-var optionNC17 = document.getElementById("NC17");
+//The API key for OMDb provided below.
+var OMDbAPIKey = "4145ebcc";
+
+//An example of an actual search URL provided below
+//http://www.omdbapi.com/?i=tt3896198&apikey=401e702d
 
 
-//var OMDbAPIKey = "http://www.omdbapi.com/?apikey=4145ebcc";
-var OMDbAPIKey = "apikey=4145ebcc";
-
-// The array provided below is used to store the audience(watching with) options
-
-var chooseAudience = function(event) {
+var watchMovie = function(event) {
     event.preventDefault();
-    //var audience = getElementById("audience").value;
-    var OMDbAPIUrl = "http://www.omdbapi.com/?" + "apikey=" + OMDbAPIKey;
+    var movieTitle = getElementById("search-input").value;
+    //The OMDb API URL provided below.
+    var OMDbAPIUrl = "http://www.omdbapi.com/?" + movieTitle + "apikey=" + OMDbAPIKey;
+
+    //The OMDb Poster API URL provided below.
+    var OMDbPosterAPIUrl = "http://img.omdbapi.com/?" + "apikey=" + OMDbAPIKey;
+
 }
+
